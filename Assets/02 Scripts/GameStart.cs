fcd1efe2ed5de5f4480ae1ab65826ddabc34b18f
Roguree <touko1996@gmail.com>
2025-10-17 09:53:54 +0900

    feat/GameStartUI commit

---
+++ Assets/02 Scripts/GameStart.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour
+{
+    public GameObject startImage;
+    private bool gameStarted = false;
+    
+    
+    void Start()
+    {
+        Time.timeScale = 0f;
+        startImage.SetActive(true);
+
+    }
+
+    
+    void Update()
+    {
+        if (!gameStarted && Input.GetMouseButtonDown(0))
+        {
+            StartGame();
+        }
+
+    }
+    void StartGame()
+    {
+        startImage.SetActive(false);
+
+        Time.timeScale = 1f;
+        gameStarted = true;
+    }
+}
